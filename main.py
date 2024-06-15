e0631413483bc847a96b25387460cc79b826c199
donttalk1005 <125094649+donttalk1005@users.noreply.github.com>
2024-06-16 01:37:44 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.utils.markdown import hlink
+from aiogram.types import InputMediaPhoto, InlineKeyboardButton, InlineKeyboardMarkup
 
 # Replace this with your actual bot token
 token = "7039946168:AAHB9pqu3r4UYOVp05q2k7DfNE2H7DApWOY"
